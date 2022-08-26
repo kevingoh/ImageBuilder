c7827833661c5e015c364317322cf363f2cb73e5
shpraka <46188819+shpraka@users.noreply.github.com>
2022-08-26 07:41:14 +0530

    Removing ruby changes

--- GenerateDockerFiles/ruby/template/init_container.sh
+++ GenerateDockerFiles/ruby/template/init_container.sh
@@ -18,10 +18,8 @@ cat /etc/motd
 # Get environment variables to show up in SSH session
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
-if [ "$WEBSITE_SSH_ENABLED" == "1" ]; then
-        sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
-        service ssh start
-fi
+sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
+service ssh start
 
 # Get environment variables to show up in SSH session
 eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
