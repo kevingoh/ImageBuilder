7e1844704077bc5456ab88ca582e727dcbf7cd33
ZubaeyrMSFT <zmohammed@microsoft.com>
2022-11-29 11:36:45 +0530

    updating perms

--- GenerateDockerFiles/wordpress/wordpress/local_bin/perms-service.sh
+++ GenerateDockerFiles/wordpress/wordpress/local_bin/perms-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #chmod -R 777 /var/www/wordpress
 #chown -R nginx:nginx /var/www/wordpress
-find /var/www/wordpress/* \! -perm 777 -exec chmod 777 {} \;
 find /var/www/wordpress/* \( \! -user nginx -o \! -group nginx \) -a -exec chown nginx:nginx {} \;
+find /var/www/wordpress/* \! -perm 777 -exec chmod 777 {} \;
