2ff5b94110e52d6fb308eb8b52af1ddece0a3630
ZubaeyrMSFT <zmohammed@microsoft.com>
2022-12-04 20:07:27 +0530

    removing test case

--- GenerateDockerFiles/wordpress/wordpress/local_bin/unison-cleanup-service.sh
+++ GenerateDockerFiles/wordpress/wordpress/local_bin/unison-cleanup-service.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-date >> /home/dev/unisoncleanup.txt
 find $WORDPRESS_HOME/ -name "*.unison.tmp" -mtime +1 -exec rm -rf {} 2> /dev/null \;
 find $HOME_SITE_LOCAL_STG/ -name "*.unison.tmp" -mtime +1 -exec rm -rf {} 2> /dev/null \;
