e3171eb7dfee8244141aa02ca11bae2744a00b5a
ZubaeyrMSFT <zmohammed@microsoft.com>
2023-05-19 14:24:50 +0530

    disabling subdomain multisite installation with afd

--- GenerateDockerFiles/wordpress/wordpress/local_bin/entrypoint.sh
+++ GenerateDockerFiles/wordpress/wordpress/local_bin/entrypoint.sh
@@ -390,51 +390,22 @@ if [ -e "$WORDPRESS_HOME/wp-config.php" ]; then
 fi
 
 
-convert_multisite() {
-
-    if [[ "$WORDPRESS_MULTISITE_TYPE" == "subdirectory" ]]; then
-        if wp plugin deactivate --all --path=$WORDPRESS_HOME --allow-root \
-        && wp core multisite-convert --url=$WEBSITE_HOSTNAME --path=$WORDPRESS_HOME --allow-root; then
-
-            # Removing duplicate occurance of DOMAIN_CURRENT_SITE
-            wp config delete DOMAIN_CURRENT_SITE --path=$WORDPRESS_HOME --allow-root 2> /dev/null;
-            wp config set DOMAIN_CURRENT_SITE \$_SERVER[\'HTTP_HOST\'] --raw --path=$WORDPRESS_HOME --allow-root 2> /dev/null;
-            echo "MULTISITE_CONVERSION_COMPLETED" >> $WORDPRESS_LOCK_FILE
-        fi
-
-    elif [[ "$WORDPRESS_MULTISITE_TYPE" == "subdomain" ]]; then
-        MULTISITE_DOMAIN=$WEBSITE_HOSTNAME
-        if [[ $MULTISITE_CUSTOM_DOMAIN ]]; then
-            MULTISITE_DOMAIN=$MULTISITE_CUSTOM_DOMAIN
-        elif [[ $AFD_ENABLED ]] && [[ "$AFD_ENABLED" == "true" || "$AFD_ENABLED" == "TRUE" \
-            || "$AFD_ENABLED" == "True" ]] && [[ $AFD_CUSTOM_DOMAIN ]]; then
-            MULTISITE_DOMAIN=$AFD_CUSTOM_DOMAIN
-        fi
-
-        if [[ $MULTISITE_DOMAIN != $WEBSITE_HOSTNAME ]]; then
-            if wp plugin deactivate --all --path=$WORDPRESS_HOME --allow-root \
-            && wp core multisite-convert --subdomains --url=$MULTISITE_DOMAIN --path=$WORDPRESS_HOME --allow-root \
-            && wp option update siteurl "https://$MULTISITE_DOMAIN" --path=$WORDPRESS_HOME --allow-root \
-            && wp option update home "https://$MULTISITE_DOMAIN" --path=$WORDPRESS_HOME --allow-root; then
-
-                # Removing duplicate occurance of DOMAIN_CURRENT_SITE
-                wp config delete DOMAIN_CURRENT_SITE --path=$WORDPRESS_HOME --allow-root 2> /dev/null;
-                wp config set DOMAIN_CURRENT_SITE \$_SERVER[\'HTTP_HOST\'] --raw --path=$WORDPRESS_HOME --allow-root 2> /dev/null;
-                echo "MULTISITE_CONVERSION_COMPLETED" >> $WORDPRESS_LOCK_FILE
-            fi
-        fi
-    fi
-}
-
 # Multi-site conversion
 if [[ $(grep "WP_INSTALLATION_COMPLETED" $WORDPRESS_LOCK_FILE) ]] && [[ ! $(grep "MULTISITE_CONVERSION_COMPLETED" $WORDPRESS_LOCK_FILE) ]] \
     && [[ $WORDPRESS_MULTISITE_CONVERT ]] && [[ "$WORDPRESS_MULTISITE_CONVERT" == "true" || "$WORDPRESS_MULTISITE_CONVERT" == "TRUE" || "$WORDPRESS_MULTISITE_CONVERT" == "True" ]] \
-    && [[ $WORDPRESS_MULTISITE_TYPE ]] && [[ "$WORDPRESS_MULTISITE_TYPE" == "subdirectory" || "$WORDPRESS_MULTISITE_TYPE" == "subdomain" ]]; then
+    && [[ $WORDPRESS_MULTISITE_TYPE ]] && [[ "$WORDPRESS_MULTISITE_TYPE" == "subdirectory" || "$WORDPRESS_MULTISITE_TYPE" == "subdomain" ]] \
+    && [[ ! "$(wp config get WP_ALLOW_MULTISITE --path=$WORDPRESS_HOME --allow-root 2> /dev/null)" ]] then
+
+    IS_AFD_ENABLED="False"
+    if [[ $AFD_ENABLED ]] && [[ "$AFD_ENABLED" == "true" || "$AFD_ENABLED" == "TRUE" \
+        || "$AFD_ENABLED" == "True" ]] && [[ $AFD_ENDPOINT ]]; then
+    	IS_AFD_ENABLED="True"
+    fi
 
     # There is an issue with AFD where $_SERVER['HTTP_HOST'] header is still pointing to <sitename>.azurewebsites.net instead of AFD endpoint.
     # This is causing database connection issue with multi-site WordPress because the main site domain (AFD endpoint) doesn't match the one in HTTP_HOST header.
     if [ $(grep "BLOB_AFD_CONFIGURATION_COMPLETE" $WORDPRESS_LOCK_FILE) ] || [ $(grep "AFD_CONFIGURATION_COMPLETE" $WORDPRESS_LOCK_FILE) ]; then
-        if [[ $AFD_ENABLED ]] && [[ "$AFD_ENABLED" == "true" || "$AFD_ENABLED" == "TRUE" || "$AFD_ENABLED" == "True" ]]; then
+        if [[ "$IS_AFD_ENABLED" == "True" ]]; then
             wp config set WP_HOME "\$http_protocol . \$_SERVER['HTTP_HOST']" --raw --path=$WORDPRESS_HOME --allow-root
             wp config set WP_SITEURL "\$http_protocol . \$_SERVER['HTTP_HOST']" --raw --path=$WORDPRESS_HOME --allow-root
         fi
@@ -450,7 +421,29 @@ if [[ $(grep "WP_INSTALLATION_COMPLETED" $WORDPRESS_LOCK_FILE) ]] && [[ ! $(grep
         IS_SMUSHIT_ENABLED="True"
     fi
 
-    convert_multisite
+    MULTISITE_DOMAIN=$WEBSITE_HOSTNAME
+    if [[ $MULTISITE_CUSTOM_DOMAIN && "$WORDPRESS_MULTISITE_TYPE" == "subdomain" ]]; then
+        MULTISITE_DOMAIN=$MULTISITE_CUSTOM_DOMAIN
+
+    ADD_SUBDOMAIN_FLAG=''
+    if [[ "$WORDPRESS_MULTISITE_TYPE" == "subdomain" ]]
+        ADD_SUBDOMAIN_FLAG='true'
+    fi
+
+    if [[ "$WORDPRESS_MULTISITE_TYPE" == "subdomain" && $MULTISITE_CUSTOM_DOMAIN != $WEBSITE_HOSTNAME && $IS_AFD_ENABLED == "False" ]] \
+        || [[ "$WORDPRESS_MULTISITE_TYPE" == "subdirectory" ]]; then
+
+        if wp plugin deactivate --all --path=$WORDPRESS_HOME --allow-root \
+        && wp core multisite-convert ${ADD_SUBDOMAIN_FLAG:+--subdomains} --url=$MULTISITE_DOMAIN --path=$WORDPRESS_HOME --allow-root \
+        && wp option update siteurl "https://$MULTISITE_DOMAIN" --path=$WORDPRESS_HOME --allow-root \
+        && wp option update home "https://$MULTISITE_DOMAIN" --path=$WORDPRESS_HOME --allow-root; then
+
+            # Removing duplicate occurance of DOMAIN_CURRENT_SITE
+            wp config delete DOMAIN_CURRENT_SITE --path=$WORDPRESS_HOME --allow-root 2> /dev/null;
+            wp config set DOMAIN_CURRENT_SITE \$_SERVER[\'HTTP_HOST\'] --raw --path=$WORDPRESS_HOME --allow-root 2> /dev/null;
+            echo "MULTISITE_CONVERSION_COMPLETED" >> $WORDPRESS_LOCK_FILE
+        fi
+    fi
 
     #Re-activate W3TC & SmushIt plugins
     if [[ "$IS_W3TC_ENABLED" == "True" ]]; then
